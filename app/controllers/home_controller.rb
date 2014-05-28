cf751a30ccf7177e7761b202d942e1b95c44c96f
EzGame <zyq.david@gmail.com>
2014-05-28 00:09:48 -0400

    Extremely bad verison of infinite scroll

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,17 +6,14 @@ class HomeController < ApplicationController
   # Search for mentions
   def search
   	# Process search based on param, or a clean search
-    if ( params[:cmd] == 'next' )   
-      request_results = $client.search($username, :result_type => 'recent', :max_id => $last_id, :count => 5)
-
-    elsif ( params[:cmd] == 'prev')
-      request_results = $client.search($username, :result_type => 'recent', :since_id => $first_id, :count => 5)
+    if ( params[:cmd] == 'more' )   
+      request_results = $client.search($username, :result_type => 'recent', :max_id => $last_id - 1)
 
     else
       # Save username for this search
       $username = params[:home][:username]
-      request_results = $client.search($username, :result_type => 'recent', :count => 5)
-
+      $mentions = []
+      request_results = $client.search('@' + $username, :result_type => 'recent')
     end
 
     # Process results
@@ -25,15 +22,15 @@ class HomeController < ApplicationController
     request_results.each do |tweet|
   	  tweetIDs.push(tweet.id)
   	  @tweetCount += 1
+
+  	  # Because twitter is stupid
+  	  break unless @tweetCount < 5
   	end
 
-  	# Save some variables
-  	$first_id = tweetIDs.first
+  	# Save the last id
   	$last_id = tweetIDs.last
 
-  	puts $first_id
-  	puts $last_id
-
-  	@mentions = $client.oembeds(tweetIDs)
+  	# Get the embedded tweets
+  	$mentions += $client.oembeds(tweetIDs)
   end
 end
